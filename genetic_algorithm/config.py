cbf032c24d93124b785e6ff70e45dd5f0177ccc4
skalbania2105 <skalbania@student.agh.edu.pl>
2025-03-09 20:11:06 +0100

    Dodanie pliku config

---
+++ genetic_algorithm/config.py
@@ -0,0 +1,34 @@
+# Konfiguracja algorytmu genetycznego
+
+# Liczba osobników w populacji
+POPULATION_SIZE = 100  
+
+# Długość chromosomu (liczba bitów na zmienną)
+CHROMOSOME_LENGTH = 20  
+
+# Liczba epok (iteracji)
+NUM_GENERATIONS = 200  
+
+# Prawdopodobieństwo krzyżowania
+CROSSOVER_PROB = 0.8  
+
+# Prawdopodobieństwo mutacji
+MUTATION_PROB = 0.05  
+
+# Prawdopodobieństwo inwersji
+INVERSION_PROB = 0.02  
+
+# Liczba najlepszych osobników przechodzących do kolejnej populacji (elitism)
+ELITISM_COUNT = 5  
+
+# Rodzaj selekcji ("best", "roulette", "tournament")
+SELECTION_METHOD = "roulette"  
+
+# Rodzaj krzyżowania ("one_point", "two_point", "uniform", "granular")
+CROSSOVER_METHOD = "one_point"  
+
+# Rodzaj mutacji ("boundary", "one_point", "two_point")
+MUTATION_METHOD = "one_point"  
+
+# Funkcja testowa do optymalizacji (np. "rastrigin", "ackley")
+TEST_FUNCTION = "rastrigin"  
