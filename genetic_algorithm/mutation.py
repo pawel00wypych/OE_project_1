47654180bbc4710427aa63115364b5d395562c42
Paweł Wypych <pawelwypych4@gmail.com>
2025-03-14 23:06:05 +0100

    mutation functions templates

--- genetic_algorithm/mutation.py
+++ genetic_algorithm/mutation.py
@@ -2,3 +2,15 @@
 class  Mutation:
     def __init__(self):
         pass
+
+    def single_point_mutation(self):
+        pass
+
+    def two_point_mutation(self):
+        pass
+
+    def edge_mutation(self):
+        pass
+
+    def mutation_probability_config(self):
+        pass
