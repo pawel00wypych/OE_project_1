3a2eeaf31c38f64d39260d9a3f2ab3e8654b2f08
skalbania2105 <skalbania@student.agh.edu.pl>
2025-03-15 21:54:48 +0100

    add main loop

--- genetic_algorithm/main.py
+++ genetic_algorithm/main.py
@@ -1,4 +1,18 @@
+from population import Population
+from config import MAX_GENERATIONS
 
+pop = Population(number_of_variables=10, precision=3, variables_list=[(-5,5)]*10, function_name="sphere")
 
-if __name__ == '__main__':
-    print("Hello World")
+# Algorytm genetyczny - główna pętla
+for generation in range(MAX_GENERATIONS):
+    pop.evaluate()  # Ocena fitness populacji
+    pop.select()    # Selekcja rodziców
+    pop.crossover() # Krzyżowanie
+    pop.mutate()    # Mutacja
+    pop.apply_elitism()  # Strategia elitarna
+
+    # Pobranie najlepszego osobnika w tej generacji
+    best = pop.get_best()
+    print(f"Generacja {generation+1}: Najlepszy wynik = {best.fitness}")
+
+print("Optymalizacja zakończona.")
