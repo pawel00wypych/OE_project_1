66f3d54c200d53bf10cabc73ed0bc417b00752df
Milosz180 <129399229+Milosz180@users.noreply.github.com>
2025-03-23 18:30:08 +0100

    adding option to choose - min or max fitness in selection

--- genetic_algorithm/unit_tests/selection_test.py
+++ genetic_algorithm/unit_tests/selection_test.py
@@ -16,6 +16,7 @@ if __name__ == "__main__":
     num_variables = 3
     precision = 2
     variables_list = [(0, 5), (-1, 7), (2, 8)]
+    selection_type = "min"
 
     individuals = []
     for _ in range(population_size):
@@ -34,18 +35,18 @@ if __name__ == "__main__":
 
     # Testing best selection
     print("\nBest Selection:")
-    best_individuals = Selection.best_selection(population, num_selected=3)
+    best_individuals = Selection.best_selection(population, num_selected=3, selection_type=selection_type)
     for individual in best_individuals:
         print(f"Fitness: {individual.fitness}, Decoded: {individual.decoded_variables}")
 
     # Testing roulette selection
     print("\nRoulette Selection:")
-    selected_by_roulette = Selection.roulette_selection(population, num_selected=3)
+    selected_by_roulette = Selection.roulette_selection(population, num_selected=3, selection_type=selection_type)
     for individual in selected_by_roulette:
         print(f"Fitness: {individual.fitness}, Decoded: {individual.decoded_variables}")
 
     # Testing tournament selection
     print("\nTournament Selection:")
-    selected_by_tournament = Selection.tournament_selection(population, num_selected=3)
+    selected_by_tournament = Selection.tournament_selection(population, num_selected=3, selection_type=selection_type)
     for individual in selected_by_tournament:
         print(f"Fitness: {individual.fitness}, Decoded: {individual.decoded_variables}")
