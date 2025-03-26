b993592d2b9a50090e52f0853db48c9f83b40c75
unknown <pawelwypych4@gmail.com>
2025-03-26 23:04:36 +0100

    added 2 new functions

--- genetic_algorithm/evaluation_functions.py
+++ genetic_algorithm/evaluation_functions.py
@@ -1,9 +1,9 @@
-from benchmark_functions import Hypersphere # Sphere Function
-from opfunu.cec_based.cec2014 import F12014  # Hybrid Function 1
+from benchmark_functions import Hypersphere, Rana # Sphere Function
+from opfunu.cec_based.cec2014 import F12014, F282014   # Hybrid Function 1
 
 # 📌 Sphere function - 2 variables
 hypersphere_function = Hypersphere()
-hypersphere_function_minimum = hypersphere_function.minima
+
 
 def hypersphere_fitness(variables):
     return hypersphere_function._evaluate(variables)
@@ -13,6 +13,27 @@ def get_hypersphere_minimum():
 
 def get_hypersphere_maximum():
     return hypersphere_function.maximum()
+
+# rana function - 2 variables
+rana_function = Rana()
+
+def rana_fitness(variables):
+    return rana_function._evaluate(variables)
+
+def get_rana_minimum():
+    return rana_function.minimum()
+
+def get_rana_maximum():
+    return rana_function.maximum()
+
+# Composition function 6 F282014 - 30 variables
+composition_6_function = F282014()
+
+def composition_6_fitness(variables):
+    return composition_6_function.evaluate(variables)
+
+def get_cec_composition_6_minimum():
+    return composition_6_function.f_global, tuple(composition_6_function.x_global)
 
 # 📌 Hybrid Function 1 (Schwefel + Rastrigin + Elliptic)
 hybrid_function = F12014()
